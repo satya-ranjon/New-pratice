7b7922c07d7e3905033bc45a0efbd3d1d0645f45
Satya Ranjon <satyaranjonofficial@gmail.com>
2022-02-17 11:53:19 +0600

    Class12

--- src/components/class13/clickCount.js
+++
@@ -1,6 +0,0 @@
-import WithCounter from "./HOC/withCounter";
-function clickCount({ handelCount, count }) {
-  return <button onClick={handelCount}> Click : {count}</button>;
-}
-
-export default WithCounter(clickCount);
--- src/components/class13/hoverCount.js
+++
@@ -1,7 +0,0 @@
-import WithCounter from "./HOC/withCounter";
-
-function hoverCount({ handelCount, count }) {
-  return <h3 onMouseOver={handelCount}>Hover Count : {count}</h3>;
-}
-
-export default WithCounter(hoverCount);
