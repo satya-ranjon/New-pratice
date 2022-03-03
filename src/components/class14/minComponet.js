371c7ad63a4f738c4096ff50d74c34f756335250
Satya Ranjon <satyaranjonofficial@gmail.com>
2022-03-03 15:12:30 +0600

    Complate

--- src/components/class14/minComponet.js
+++ src/components/class14/minComponet.js
@@ -7,24 +7,26 @@ import ThemeContext from "./context/context";
 export default class minComponent extends React.Component {
   state = {
     theme: "dark",
+    handelTheme: () => {
+      this.setState(({ theme }) => {
+        if (theme === "dark") {
+          return {
+            theme: "light",
+          };
+        }
+        return {
+          theme: "dark",
+        };
+      });
+    },
   };
 
-  handelTheme = () => {
-    this.setState(({ theme }) => {
-      if (theme === "dark") {
-        return {
-          theme: "light",
-        };
-      }
-      return {
-        theme: "dark",
-      };
-    });
-  };
   render() {
     return (
       <>
-        <h2 style={{ textAlign: "center" }}>This is Rander Props</h2>
+        <h2 style={{ textAlign: "center" }}>
+          This is React Context API - contextType & useContext hook
+        </h2>
 
         <Counter>
           {(count, handelCount) => (
@@ -32,7 +34,10 @@ export default class minComponent extends React.Component {
           )}
         </Counter>
         <ThemeContext.Provider
-          value={{ theme: this.state.theme, handelTheme: this.handelTheme }}
+          value={{
+            theme: this.state.theme,
+            handelTheme: this.state.handelTheme,
+          }}
         >
           <Section />
         </ThemeContext.Provider>
